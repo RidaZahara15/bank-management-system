e3261de7836675c41796ea20b75dd57e8d39664b
Rida Zahara <rz3920800@gmail.com>
2026-08-09 00:58:38 -0700

    User schema and create user endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app import models
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app import schemas 
+from fastapi import Depends
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Bank Management API")
+def get_db():
+    db = SessionLocal()   
+    try:
+        yield db         #give this session to endpoint for use
+    finally:
+        db.close() 
 
 
 @app.get("/")
@@ -15,3 +25,17 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+
+
+@app.post("/users", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=user.name,
+        email=user.email,
+        password_hash=user.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
