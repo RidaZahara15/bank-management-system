56d323c3957da5ae4ce9bdcaac0e4d4289a7b10c
Rida Zahara <rz3920800@gmail.com>
2026-08-13 23:45:23 -0700

    Authorization completed

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,7 @@
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
+from jose import JWTError
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -15,7 +16,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 
 
-SECRET_KEY = "yeh-ek-secret-key-hai-abhi-ke-liye"
+SECRET_KEY = "This is secret key for now"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
@@ -25,3 +26,13 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ from app import schemas
 from fastapi import Depends
 from app import auth
 from fastapi import HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -16,7 +18,25 @@ def get_db():
     try:
         yield db         #give this session to endpoint for use
     finally:
-        db.close() 
+        db.close()
+
+
+
+
+security = HTTPBearer()
+
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
+    token = credentials.credentials
+    payload = auth.verify_token(token)
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    user = db.query(models.User).filter(models.User.id == payload.get("user_id")).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
 
 
 @app.get("/")
@@ -56,3 +76,9 @@ def login(credentials: schemas.UserLogin, db: Session = Depends(get_db)):
 
     token = auth.create_access_token(data={"user_id": user.id, "email": user.email})
     return {"access_token": token, "token_type": "bearer"}
+
+
+
+@app.get("/profile", response_model=schemas.UserResponse)
+def get_profile(current_user: models.User = Depends(get_current_user)):
+    return current_user
